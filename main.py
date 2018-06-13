036545fff3087b1130709dc2a352a24d6d217f07
BenSNW <zpkx_wang77@163.com>
2018-06-13 19:24:37 +0800

    evaluate file

--- main.py
+++ main.py
@@ -1,53 +1,62 @@
 # encoding=utf8
-import os
-import codecs
-import pickle
 import itertools
+import os
+import pickle
 from collections import OrderedDict
 
-import tensorflow as tf
 import numpy as np
-from model import Model
-from loader import load_sentences, update_tag_scheme
-from loader import char_mapping, tag_mapping
+import tensorflow as tf
+
+from data_utils import load_word2vec, input_from_line, BatchManager
 from loader import augment_with_pretrained, prepare_dataset
+from loader import char_mapping, tag_mapping
+from loader import load_sentences, update_tag_scheme
+from model import NERModel
 from utils import get_logger, make_path, clean, create_model, save_model
 from utils import print_config, save_config, load_config, test_ner
-from data_utils import load_word2vec, create_input, input_from_line, BatchManager
 
 flags = tf.app.flags
-flags.DEFINE_boolean("clean",       False,      "clean train folder")
-flags.DEFINE_boolean("train",       False,      "Wither train the model")
+flags.DEFINE_boolean("clean", False, "clean train folder")
+flags.DEFINE_boolean("train", False, "Wither train the model")
 # configurations for the model
-flags.DEFINE_integer("seg_dim",     20,         "Embedding size for segmentation, 0 if not used")
-flags.DEFINE_integer("char_dim",    100,        "Embedding size for characters")
-flags.DEFINE_integer("lstm_dim",    100,        "Num of hidden units in LSTM")
-flags.DEFINE_string("tag_schema",   "iobes",    "tagging schema iobes or iob")
+# flags.DEFINE_integer("seg_dim", 20, "Embedding size for segmentation, 0 if not used")
+flags.DEFINE_integer("seg_dim", 4, "Embedding size for segmentation, 0 if not used")
+# flags.DEFINE_integer("char_dim", 100, "Embedding size for characters")
+flags.DEFINE_integer("char_dim", 80, "Embedding size for characters")
+# flags.DEFINE_integer("lstm_dim", 100, "Num of hidden units in LSTM")
+flags.DEFINE_integer("lstm_dim", 80, "Num of hidden units in LSTM")
+flags.DEFINE_string("tag_schema", "iobes", "tagging schema iobes or iob")
 
 # configurations for training
-flags.DEFINE_float("clip",          5,          "Gradient clip")
-flags.DEFINE_float("dropout",       0.5,        "Dropout rate")
-flags.DEFINE_float("batch_size",    20,         "batch size")
-flags.DEFINE_float("lr",            0.001,      "Initial learning rate")
-flags.DEFINE_string("optimizer",    "adam",     "Optimizer for training")
-flags.DEFINE_boolean("pre_emb",     True,       "Wither use pre-trained embedding")
-flags.DEFINE_boolean("zeros",       False,      "Wither replace digits with zero")
-flags.DEFINE_boolean("lower",       True,       "Wither lower case")
+flags.DEFINE_float("clip", 5, "Gradient clip")
+flags.DEFINE_float("dropout", 0.5, "Dropout rate")
+flags.DEFINE_integer("batch_size", 20, "batch size")
+flags.DEFINE_float("lr", 0.001, "Initial learning rate")
+flags.DEFINE_string("optimizer", "adam", "Optimizer for training")
+flags.DEFINE_boolean("pre_emb", True, "Wither use pre-trained embedding")
+# flags.DEFINE_boolean("zeros",       False,      "Wither replace digits with zero")
+flags.DEFINE_boolean("zeros", True, "Wither replace digits with zero")
+# flags.DEFINE_boolean("lower", True, "Wither lower case")
+flags.DEFINE_boolean("lower", False, "Wither lower case")
 
-flags.DEFINE_integer("max_epoch",   100,        "maximum training epochs")
-flags.DEFINE_integer("steps_check", 100,        "steps per checkpoint")
-flags.DEFINE_string("ckpt_path",    "ckpt",      "Path to save model")
-flags.DEFINE_string("summary_path", "summary",      "Path to store summaries")
-flags.DEFINE_string("log_file",     "train.log",    "File for log")
-flags.DEFINE_string("map_file",     "maps.pkl",     "file for maps")
-flags.DEFINE_string("vocab_file",   "vocab.json",   "File for vocab")
-flags.DEFINE_string("config_file",  "config_file",  "File for config")
-flags.DEFINE_string("script",       "conlleval",    "evaluation script")
-flags.DEFINE_string("result_path",  "result",       "Path for results")
-flags.DEFINE_string("emb_file",     "wiki_100.utf8", "Path for pre_trained embedding")
-flags.DEFINE_string("train_file",   os.path.join("data", "example.train"),  "Path for train data")
-flags.DEFINE_string("dev_file",     os.path.join("data", "example.dev"),    "Path for dev data")
-flags.DEFINE_string("test_file",    os.path.join("data", "example.test"),   "Path for test data")
+# flags.DEFINE_integer("max_epoch",   100,        "maximum training epochs")
+flags.DEFINE_integer("max_epoch", 12, "maximum training epochs")
+flags.DEFINE_integer("steps_check", 100, "steps per checkpoint")
+flags.DEFINE_string("ckpt_path", "ckpt", "Path to save model")
+flags.DEFINE_string("summary_path", "summary", "Path to store summaries")
+flags.DEFINE_string("log_file", "train.log", "File for log")
+flags.DEFINE_string("map_file", "maps.pkl", "file for maps")
+flags.DEFINE_string("vocab_file", "vocab.json", "File for vocab")
+flags.DEFINE_string("config_file", "config_file", "File for config")
+flags.DEFINE_string("script", "conlleval", "evaluation script")
+flags.DEFINE_string("result_path", "result", "Path for results")
+flags.DEFINE_string("emb_file", "wiki_100.utf8", "Path for pre_trained embedding")
+# flags.DEFINE_string("train_file", os.path.join("data", "example.train"), "Path for train data")
+# flags.DEFINE_string("dev_file", os.path.join("data", "example.dev"), "Path for dev data")
+# flags.DEFINE_string("test_file", os.path.join("data", "example.test"), "Path for test data")
+flags.DEFINE_string("train_file", 'C:/Document/intellij/twarp-kg/data/industry-ner-train-argumented.txt', "Path for train data")
+flags.DEFINE_string("dev_file", 'C:/Document/intellij/twarp-kg/data/industry-ner-train.txt', "Path for dev data")
+flags.DEFINE_string("test_file", 'C:/Document/intellij/twarp-kg/data/industry-ner-train.txt', "Path for test data")
 
 
 FLAGS = tf.app.flags.FLAGS
@@ -115,14 +124,12 @@ def train():
     if not os.path.isfile(FLAGS.map_file):
         # create dictionary for word
         if FLAGS.pre_emb:
-            dico_chars_train = char_mapping(train_sentences, FLAGS.lower)[0]
-            dico_chars, char_to_id, id_to_char = augment_with_pretrained(
-                dico_chars_train.copy(),
+            dict_chars_train = char_mapping(train_sentences, FLAGS.lower)[0]
+            dict_chars, char_to_id, id_to_char = augment_with_pretrained(
+                dict_chars_train.copy(),
                 FLAGS.emb_file,
                 list(itertools.chain.from_iterable(
-                    [[w[0] for w in s] for s in test_sentences])
-                )
-            )
+                    [[w[0] for w in s] for s in test_sentences])))
         else:
             _c, char_to_id, id_to_char = char_mapping(train_sentences, FLAGS.lower)
 
@@ -135,17 +142,12 @@ def train():
             char_to_id, id_to_char, tag_to_id, id_to_tag = pickle.load(f)
 
     # prepare data, get a collection of list containing index
-    train_data = prepare_dataset(
-        train_sentences, char_to_id, tag_to_id, FLAGS.lower
-    )
-    dev_data = prepare_dataset(
-        dev_sentences, char_to_id, tag_to_id, FLAGS.lower
-    )
+    train_data = prepare_dataset(train_sentences, char_to_id, tag_to_id, FLAGS.lower)
+    dev_data = prepare_dataset(dev_sentences, char_to_id, tag_to_id, FLAGS.lower)
     test_data = prepare_dataset(
         test_sentences, char_to_id, tag_to_id, FLAGS.lower
     )
-    print("%i / %i / %i sentences in train / dev / test." % (
-        len(train_data), 0, len(test_data)))
+    print("%i / %i / %i sentences in train / dev / test." % (len(train_data), len(dev_data), len(test_data)))
 
     train_manager = BatchManager(train_data, FLAGS.batch_size)
     dev_manager = BatchManager(dev_data, 100)
@@ -168,18 +170,17 @@ def train():
     tf_config.gpu_options.allow_growth = True
     steps_per_epoch = train_manager.len_data
     with tf.Session(config=tf_config) as sess:
-        model = create_model(sess, Model, FLAGS.ckpt_path, load_word2vec, config, id_to_char, logger)
+        model = create_model(sess, NERModel, FLAGS.ckpt_path, load_word2vec, config, id_to_char, logger)
         logger.info("start training")
         loss = []
-        for i in range(100):
+        for i in range(FLAGS.max_epoch):
             for batch in train_manager.iter_batch(shuffle=True):
                 step, batch_loss = model.run_step(sess, True, batch)
                 loss.append(batch_loss)
                 if step % FLAGS.steps_check == 0:
                     iteration = step // steps_per_epoch + 1
-                    logger.info("iteration:{} step:{}/{}, "
-                                "NER loss:{:>9.6f}".format(
-                        iteration, step%steps_per_epoch, steps_per_epoch, np.mean(loss)))
+                    logger.info("iteration:{} step:{}/{}, ""NER loss:{:>9.6f}".format(
+                        iteration, step % steps_per_epoch, steps_per_epoch, np.mean(loss)))
                     loss = []
 
             best = evaluate(sess, model, "dev", dev_manager, id_to_tag, logger)
@@ -197,7 +198,7 @@ def evaluate_line():
     with open(FLAGS.map_file, "rb") as f:
         char_to_id, id_to_char, tag_to_id, id_to_tag = pickle.load(f)
     with tf.Session(config=tf_config) as sess:
-        model = create_model(sess, Model, FLAGS.ckpt_path, load_word2vec, config, id_to_char, logger)
+        model = create_model(sess, NERModel, FLAGS.ckpt_path, load_word2vec, config, id_to_char, logger)
         while True:
             # try:
             #     line = input("请输入测试句子:")
@@ -206,13 +207,69 @@ def evaluate_line():
             # except Exception as e:
             #     logger.info(e)
 
-                line = input("请输入测试句子:")
-                result = model.evaluate_line(sess, input_from_line(line, char_to_id), id_to_tag)
+            line = input("请输入测试句子:")
+            result = model.evaluate_line(sess, input_from_line(line, char_to_id), id_to_tag)
+            print(result)
+
+
+def evaluate_file(file):
+    config = load_config(FLAGS.config_file)
+    logger = get_logger(FLAGS.log_file)
+    # limit GPU memory
+    tf_config = tf.ConfigProto()
+    tf_config.gpu_options.allow_growth = True
+    with open(FLAGS.map_file, "rb") as f:
+        char_to_id, id_to_char, tag_to_id, id_to_tag = pickle.load(f)
+    with tf.Session(config=tf_config) as sess:
+        model = create_model(sess, NERModel, FLAGS.ckpt_path, load_word2vec, config, id_to_char, logger)
+        with open(file, encoding='u8') as f:
+            for line in f:
+                result = model.evaluate_line(sess, input_from_line(line.strip(), char_to_id), id_to_tag)
                 print(result)
 
 
-def main(_):
+def evaluate_file_save(file, save_file):
+    config = load_config(FLAGS.config_file)
+    logger = get_logger(FLAGS.log_file)
+    # limit GPU memory
+    tf_config = tf.ConfigProto()
+    tf_config.gpu_options.allow_growth = True
+    # save path
+    save_path = open(save_file, 'w')
+    with open(FLAGS.map_file, "rb") as f:
+        char_to_id, id_to_char, tag_to_id, id_to_tag = pickle.load(f)
+    with tf.Session(config=tf_config) as sess:
+        model = create_model(sess, NERModel, FLAGS.ckpt_path, load_word2vec, config, id_to_char, logger)
+        with open(file, encoding='u8') as f:
+            for line in f:
+                result = model.evaluate_line(sess, input_from_line(line.strip(), char_to_id), id_to_tag)
+                save_path.write(str(result))
+                save_path.write("\n")
 
+
+def split_file(file, limit):
+    result_list = []
+    file_count = 0
+    with open(file) as f:
+        for line in f:
+            result_list.append(line)
+            if len(result_list) < limit:
+                continue
+            file_name = "data/split/"+str(file_count)+".txt"
+            with open(file_name, 'w') as file:
+                for result in result_list[:-1]:
+                    file.write(result)
+                file.write(result_list[-1].strip())
+                result_list = []
+                file_count += 1
+    if result_list:
+        file_name = "data/split/"+str(file_count)+".txt"
+        with open(file_name, 'w') as file:
+            for result in result_list:
+                file.write(result)
+
+
+def main(_):
     if FLAGS.train:
         if FLAGS.clean:
             clean(FLAGS)
@@ -223,6 +280,4 @@ def main(_):
 
 if __name__ == "__main__":
     tf.app.run(main)
-
-
 
